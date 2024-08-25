e33a4aebc39d36a05fb2b8bb7721eec7f0fd13b3
Silver-Sword <38964705+Silver-Sword@users.noreply.github.com>
2024-08-25 16:49:56 -0400

    add user sign up to backend

---
+++ backend/src/firebase-utils/FirebaseWrapper.ts
@@ -0,0 +1,40 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth';
+import { FIREBASE_CONFIG } from '../firebaseSecrets'
+
+/*
+    Wrapper class for doing firebase stuff
+    Remember to call .initApp() before doing anything
+*/
+export default class FirebaseWrapper
+{
+    async initApp() : Promise<void>
+    {
+        if (firebase.apps.length === 0) {
+            await firebase.initializeApp(FIREBASE_CONFIG);
+        }
+    }
+
+    async signUpNewUser(email:string, password:string, displayName: string) : Promise<boolean>
+    {
+        try {
+            // user sign up
+            await firebase.auth().createUserWithEmailAndPassword(email, password);
+            const user = firebase.auth().currentUser;
+            if(!user)
+            {
+                throw Error('Something went wrong with user signup. User is null');
+            }
+    
+            // update username
+            await user.updateProfile({
+                displayName : displayName
+            });
+        } catch(error) {
+            console.log("Error creating user: ", (error as Error).message);
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ backend/src/firebaseSecrets.ts
@@ -0,0 +1,12 @@
+export const FIREBASE_CONFIG = {
+
+    projectId: "l17-tune-tracer",
+    apiKey: "AIzaSyBPV_2yOAk8JeB0cJ2OBCdJQTCJyTNWDEg",
+    authDomain: "l17-tune-tracer.firebaseapp.com",
+    storageBucket: "l17-tune-tracer.appspot.com",
+    messagingSenderId: "268123826560",
+    appId: "1:268123826560:web:a75235657488bf0d727965",
+    measurementId: "G-68VYCRF92M"
+  
+  };
+  
