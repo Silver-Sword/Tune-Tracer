f3b24ba905d6f91fe686c507cec5b20de7528204
Silver-Sword <38964705+Silver-Sword@users.noreply.github.com>
2024-09-15 11:18:20 -0400

    add function to monitor realtime updates for single documents

---
+++ backend/src/document-utils/realtimeDocumentUpdates.ts
@@ -0,0 +1,18 @@
+import FirebaseWrapper from "../firebase-utils/FirebaseWrapper";
+import { Document } from "@lib/documentTypes";
+
+// takes in the documentId and a function that is called whenever the document is updated in firebase
+// the onUpdateFn function takes in the updated document and is intended to be used to update the local
+// document version (ie use this updated document)
+export async function subscribeToDocumentUpdates(documentId: string,    
+                                                 onUpdateFn: (updatedDocument: Document) => void)
+{
+    const firebase = new FirebaseWrapper();
+    firebase.initApp();
+
+    firebase.subscribeToDocument(documentId, (snapshot) => {
+        if(snapshot.exists && snapshot.data() !== null && snapshot.data() !== undefined) {
+            onUpdateFn(snapshot.data() as Document);
+        }
+    });
+}
