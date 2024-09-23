5a49465dc4e5ef58472cc64e16d92d20a1f27bdb
Sophia DeAngelo <sophiad03@hotmail.com>
2024-09-23 12:16:15 -0400

    real time works now see if it works on 2 different computers

--- frontend/src/app/collabDemo/page.tsx
+++ frontend/src/app/collabDemo/page.tsx
@@ -1,43 +1,67 @@
 'use client'
 
-import React, { useState } from 'react';
-import FirebaseWrapper from '../../../../backend/src/firebase-utils/FirebaseWrapper'
-import { Document } from '../../../../lib/documentTypes'
-import { subscribeToDocumentUpdates } from '../../../../backend/src/document-utils/realtimeDocumentUpdates'
-import { updateDocument } from '../../../../backend/src/document-utils/documentOperations'
+import React, { useState, useEffect } from 'react';
+import FirebaseWrapper from '../../backend/src/firebase-utils/FirebaseWrapper'
+import { Document } from '../../lib/documentTypes'
+import { subscribeToDocumentUpdates } from '../../backend/src/document-utils/realtimeDocumentUpdates'
+import { updateDocument } from '../../backend/src/document-utils/documentOperations'
 
 export default function collabDemo() {
     const [isLoading, setIsLoading] = useState(false);
     const ethicalID = "niK5UsqxhL4i9TGd0twT";
-    const [text, setText] = useState('');
-    var documentTemp : Document;
-    const [documentTitle, setDocumentTitle] = useState(''); // Use React state for the document title
+    const [documentTitle, setDocumentTitle] = useState('Hello'); // Use React state for the document title
+    const [documentTemp, setDocumentTemp] = useState<Document>();
+    const [firebase, firebaseinit] = useState(null);
 
     async function StartButton () {
         setIsLoading(true);
-        const firebase = new FirebaseWrapper();
-        console.log("new wrapper");
-        await firebase.initApp();
-        console.log("initialized");
-        documentTemp = await firebase.getDocument(ethicalID);
-        console.log("hopeully have doc");
-        documentTemp.document_title = "Testin"; 
-        setDocumentTitle( "Testin");
-        updateDocument(documentTemp);
+        const firebaseW = new FirebaseWrapper();
+
+        await firebaseW.initApp();
+        // firebaseinit(firebaseW);
+
+        let realTemp = (await firebaseW.getDocument(ethicalID)) as Document;
+
+        setDocumentTemp(realTemp);
+
+        // if(documentTemp == null || documentTemp == undefined)
+        // {
+        //     throw Error(`Null document`);
+        // }
+
+        // console.log(`documentTemp=
+        //     ${documentTemp ? JSON.stringify(documentTemp) : "null"}`
+        // );
+        // setDocumentTitle(documentTemp?.document_title);
+
+        if (realTemp == null || realTemp == undefined) {
+            throw Error('Document is null');
+        }
+
+        console.log(`documentTemp= ${documentTemp ? JSON.stringify(documentTemp) : "null"}`);
+        setDocumentTitle(realTemp.document_title);
+
+        subscribeToDocumentUpdates(ethicalID, (updatedDocument: Document) => {
+            console.log(`Detected changes in document ${ethicalID}`);
+            setDocumentTitle(updatedDocument.document_title);
+            setDocumentTemp(updatedDocument); // update the temp document with new changes
+            updateDocument(updatedDocument);
+        });
+        
         setIsLoading(false);
-        return;
     };
 
     async function Changes (event: React.ChangeEvent<HTMLTextAreaElement>) {
-        const updated = event.target.value;
-        setDocumentTitle(updated);
-        documentTemp.document_title = documentTitle;
+        const updated = event.target.value as string;
+        if (documentTemp == null || documentTemp == undefined) {
+            throw Error('Document is null');
+        }
+        var updatedDocument = documentTemp as Document;
+        console.log(`Trying to update with title=${updated}`);
+        updatedDocument.document_title = updated;
+        setDocumentTemp(updatedDocument);
         // await updateDocument(documentTemp);
-        subscribeToDocumentUpdates(ethicalID, (updatedDocument: Document) => {
-            console.log(`Detected changes in document ${ethicalID}`);
-            documentTemp = updatedDocument;
-            updateDocument(documentTemp);
-        });
+        setDocumentTitle(updated);
     };
 
     return (
