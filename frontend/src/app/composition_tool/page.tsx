451a2ae4e81827a928cac5cb8ba41b934ab5af79
JASCuyugan <jascuyugan@gmail.com>
2024-10-03 18:31:49 -0400

    Initial navigation toolbar creation

--- frontend/src/app/composition_tool/page.tsx
+++ frontend/src/app/composition_tool/page.tsx
@@ -2,6 +2,17 @@
 
 import React, { useEffect, useRef, useState } from 'react';
 import { Score } from '../edit/Score';
+import {
+    AppShell, 
+    Container, 
+    Text,
+    Button,
+    Group,
+    Space,
+    Stack,
+    SimpleGrid,
+    Grid,
+} from "@mantine/core";
 
 const DEFAULT_RENDERER_WIDTH = 1000;
 const DEFAULT_RENDERER_HEIGHT = 2000;
@@ -32,8 +43,59 @@ export default function CompositionTool() {
     }, []);
 
     return (
-        <div>
-            <div ref={notationRef}></div>
-        </div>
+        <AppShell
+            header={{ height: 60 }}
+            navbar={{
+                width: 250,
+                breakpoint: "sm",
+            }}
+            padding="md"
+        >
+            <AppShell.Main>
+                <AppShell.Header
+                    style={{
+                        justifyContent: "center",
+                        display: "flex",
+                        flexDirection: "column",
+                    }}
+                >
+                    <Group justify="space-between" px="lg">
+                        <Text>Tune Tracer</Text>
+
+                        <Button component="a">Share</Button>
+                    </Group>
+                    
+                    
+                </AppShell.Header>
+                    
+                    {/* Somehow adding the NavBar removes the score and its container */}
+                {/* <AppShell.Navbar>
+
+                </AppShell.Navbar> */}
+
+                
+                    {/* get rid of the background later, use it for formatting */}
+                <Container
+                    fluid
+                    size="responsive"
+                    style={{
+                        justifyContent: "center",
+                        display: "flex",
+                        flexDirection: "column",
+                        textAlign: "center",
+                        background:
+                        "linear-gradient(180deg, rgba(154,215,255,1) 0%, rgba(0,105,182,1) 100%)",
+                    }}
+                >
+
+                    <div>
+                        <div ref={notationRef}></div>
+                    </div>
+                </Container>
+
+
+                    
+            </AppShell.Main>
+        </AppShell>
     );
 };
