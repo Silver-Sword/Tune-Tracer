280a2cf888b1c1f588a819f54e4182488378ef59
TomatoSopa <107057563+TomatoSopa@users.noreply.github.com>
2024-10-20 21:20:53 -0400

    Register login frontend branch (#51)
    * Simple CRUD API - no authority checks yet
    
    * Simple CRUD API - no authority checks yet
    
    * fix gitignore and hide key
    
    * pushing this first to avoid merge conflicts - adds functions
    
    * login and signup API working - branch is outdated though
    
    * made sure docPreviews exist
    
    * Signup in dev
    
    * Trying to make signUp work
    
    * hosting shows landing page + fixed a lot of API calling errors (still isn't working though :( )
    
    * signUp page changes and gitignore
    
    * Some debug changes
    
    * updated login / signup to actually throw errors
    
    * Progress on debugging API
    
    * Fix call :(
    
    * added all API for fireb
    
    * More testing - last attempt before switching
    
    * changes that made API work
    
    * hosting updates
    
    ---------
    
    Co-authored-by: Jordan Muniz <jordy.valois@gmail.com>

---
+++ functions/src/backend/src/document-utils/sharing/ShareCodeEntity.ts
@@ -0,0 +1,6 @@
+export type ShareCodeEntity = {
+    code: string,
+    time_created: number,
+    expiration_time: number,
+    documentId: string,
+};
---
+++ functions/src/backend/src/endpoints/createEndpoint.ts
@@ -0,0 +1,16 @@
+import FirebaseWrapper from "../firebase-utils/FirebaseWrapper";
+import firebase from 'firebase/compat/app';
+import { createDocument } from '../document-utils/documentOperations';
+import { Document } from '@lib/documentTypes';
+
+// returns new document (not just id)
+export async function createWorkspace () : Promise<Document | null>
+{
+    const user = firebase.auth().currentUser;
+    if (!user)
+    {
+        console.log("Failed to identify user");
+        return null;
+    }
+    return await createDocument(user.uid);
+}   
