a856582c8ce01488f9177e092076f1d3fdc39764
Bostonian02 <bostonian02@gmail.com>
2024-10-28 00:48:18 -0400

    Fix getAdjacentNote

--- frontend/src/app/edit/Score.tsx
+++ frontend/src/app/edit/Score.tsx
@@ -278,16 +278,22 @@ export class Score {
         let measureIndex2 = this.ID_to_MeasureIndexID.get(noteId + 1)?.measureIndex;
         if (measureIndex2 == null) return noteId;
 
-        if (measureIndex1 !== measureIndex2) {
-            if (topMeasure) {
-                return noteId + this.bottom_measures[measureIndex1].getVoice1().getTickables().length;
-            }
-            else {
-                // We know measureIndex1 + 1 
-                return noteId + this.top_measures[measureIndex1 + 1].getVoice1().getTickables().length;
-            }
+        // if (measureIndex1 !== measureIndex2) {
+        //     if (topMeasure) {
+        //         return noteId + this.bottom_measures[measureIndex1].getVoice1().getTickables().length;
+        //     }
+        //     else {
+        //         // We know measureIndex1 + 1 
+        //         return noteId + this.top_measures[measureIndex1 + 1].getVoice1().getTickables().length;
+        //     }
+        // }
+        // return noteId + 1;
+        const nextNoteId = noteId + 1;
+        if (this.ID_to_MeasureIndexID.has(nextNoteId)) {
+            return nextNoteId;
+        } else {
+            return noteId;
         }
-        return noteId + 1;
     }
 
 
