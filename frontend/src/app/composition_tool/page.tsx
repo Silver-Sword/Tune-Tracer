eb94cf18b1000bf193eec8b20a5119d037a78120
JASCuyugan <jascuyugan@gmail.com>
2024-10-29 04:12:38 -0400

    Comment aside removal and reformatting of score

--- frontend/src/app/composition_tool/page.tsx
+++ frontend/src/app/composition_tool/page.tsx
@@ -448,77 +448,77 @@ const ToolbarHeader: React.FC<{
 };
 
 // CommentCard component only used for the comment sidebar
-const CommentCard: React.FC = () => {
-    return (
-        <Paper withBorder shadow="sm" p="md" radius="md">
-            <Group>
-                {/* Need user logic for avatars, names, and date*/}
-                <Avatar radius="xl" />
-                <div>
-                    <Text fz="sm">[name]</Text>
-                    <Text fz="xs" c="dimmed">
-                        [date/time]
-                    </Text>
-                </div>
-            </Group>
-            <Text pl={54} pt="sm" size="sm">
-                Lorem ipsum
-            </Text>
-        </Paper>
-    );
-};
+// const CommentCard: React.FC = () => {
+//     return (
+//         <Paper withBorder shadow="sm" p="md" radius="md">
+//             <Group>
+//                 {/* Need user logic for avatars, names, and date*/}
+//                 <Avatar radius="xl" />
+//                 <div>
+//                     <Text fz="sm">[name]</Text>
+//                     <Text fz="xs" c="dimmed">
+//                         [date/time]
+//                     </Text>
+//                 </div>
+//             </Group>
+//             <Text pl={54} pt="sm" size="sm">
+//                 Lorem ipsum
+//             </Text>
+//         </Paper>
+//     );
+// };
 
 // Right sidebar that contains all the comments in the document
-const CommentAside: React.FC = () => {
-    const [commentInput, setCommentInput] = useState("");
+// const CommentAside: React.FC = () => {
+//     const [commentInput, setCommentInput] = useState("");
 
-    const handleComment = (event: { currentTarget: { value: any } }) => {
-        const value = event.currentTarget.value;
-        setCommentInput(value);
-        console.log(`Comment Published: ${value}`);
-        // Add more comment publishing logic here
-    };
+//     const handleComment = (event: { currentTarget: { value: any } }) => {
+//         const value = event.currentTarget.value;
+//         setCommentInput(value);
+//         console.log(`Comment Published: ${value}`);
+//         // Add more comment publishing logic here
+//     };
 
-    const handleClear = () => {
-        setCommentInput("");
-    };
+//     const handleClear = () => {
+//         setCommentInput("");
+//     };
 
-    return (
-        <AppShell.Aside withBorder p="md">
-            <Paper withBorder shadow="sm" p="md" radius="md">
-                <Stack gap="xs">
-                    <TextInput
-                        value={commentInput}
-                        onChange={(event) => setCommentInput(event.currentTarget.value)}
-                    ></TextInput>
-                    <Group>
-                        <Button color="red" onClick={handleClear}>
-                            Clear
-                        </Button>
-                        <Button onClick={handleComment}>Add Comment</Button>
-                    </Group>
-                </Stack>
-            </Paper>
-            <Space h="xs"></Space>
-            <Divider size="sm" />
-            <Space h="xs"></Space>
-            <ScrollArea scrollbarSize={4}>
-                <Stack gap="xs">
-                    <CommentCard />
-                    <CommentCard />
-                    <CommentCard />
-                    <CommentCard />
-                    <CommentCard />
-                    <CommentCard />
-                    <CommentCard />
-                    <CommentCard />
-                    <CommentCard />
-                </Stack>
-            </ScrollArea>
-            <Space h="xs"></Space>
-        </AppShell.Aside>
-    );
-};
+//     return (
+//         <AppShell.Aside withBorder p="md">
+//             <Paper withBorder shadow="sm" p="md" radius="md">
+//                 <Stack gap="xs">
+//                     <TextInput
+//                         value={commentInput}
+//                         onChange={(event) => setCommentInput(event.currentTarget.value)}
+//                     ></TextInput>
+//                     <Group>
+//                         <Button color="red" onClick={handleClear}>
+//                             Clear
+//                         </Button>
+//                         <Button onClick={handleComment}>Add Comment</Button>
+//                     </Group>
+//                 </Stack>
+//             </Paper>
+//             <Space h="xs"></Space>
+//             <Divider size="sm" />
+//             <Space h="xs"></Space>
+//             <ScrollArea scrollbarSize={4}>
+//                 <Stack gap="xs">
+//                     <CommentCard />
+//                     <CommentCard />
+//                     <CommentCard />
+//                     <CommentCard />
+//                     <CommentCard />
+//                     <CommentCard />
+//                     <CommentCard />
+//                     <CommentCard />
+//                     <CommentCard />
+//                 </Stack>
+//             </ScrollArea>
+//             <Space h="xs"></Space>
+//         </AppShell.Aside>
+//     );
+// };
 
 const DEFAULT_RENDERER_WIDTH = 1000;
 const DEFAULT_RENDERER_HEIGHT = 2000;
@@ -1406,14 +1406,14 @@ export default function CompositionTool() {
     return (
         <AppShell
             header={{ height: 180 }}
-            navbar={{
-                width: 150,
-                breakpoint: "sm",
-            }}
-            aside={{
-                width: 300,
-                breakpoint: "sm",
-            }}
+            // navbar={{
+            //     width: 150,
+            //     breakpoint: "sm",
+            // }}
+            // aside={{
+            //     width: 300,
+            //     breakpoint: "sm",
+            // }}
             padding="md"
         >
             <AppShell.Main>
@@ -1427,7 +1427,7 @@ export default function CompositionTool() {
                     onVolumeChange={setVolume}
                     addMeasure={addMeasureHandler}
                 />
-                <CommentAside />
+                {/* <CommentAside /> */}
 
                 {/* get rid of the background later, use it for formatting */}
                 <Container
