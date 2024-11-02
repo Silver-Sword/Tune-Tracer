bcbf0bb8a14288e560f4ec49a3a555e0d8f8fa49
Silver-Sword <38964705+Silver-Sword@users.noreply.github.com>
2024-11-02 19:53:14 -0400

    give proper error message for invalid invite email

--- functions/src/backend/src/lib/src/StatusCode.ts
+++ functions/src/backend/src/lib/src/StatusCode.ts
@@ -1,5 +1,6 @@
 export enum StatusCode {
     OK = 200,
     MISSING_ARGUMENTS = 400,        // the POST request is missing required arguments
+    USER_NOT_FOUND = 410,           // the user was not found in the database
     GENERAL_ERROR = 500,
 }
