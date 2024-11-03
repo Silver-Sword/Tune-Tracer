cc48aef2b27645ef28265647869cada2226a8662
Silver-Sword <38964705+Silver-Sword@users.noreply.github.com>
2024-11-02 22:47:39 -0400

    remove old/unused sharing dropdown

--- frontend/src/app/composition_tool/ToolbarHeader.tsx
+++ frontend/src/app/composition_tool/ToolbarHeader.tsx
@@ -235,13 +235,6 @@ export const ToolbarHeader: React.FC<{
         {/* Sharing UI */}
 
         {/* Select Dropdown should not be changable if not the owner */}
-        <Select
-          placeholder="Select Sharing Mode"
-          onChange={(value) => handleModeChange(value)}
-          allowDeselect={false}
-          withCheckIcon={false}
-          style={{ width: 125, marginLeft: "10px" }}
-        />
         <SharingModal
           documentTitle={inputValue}
           metadata={documentMetadata}
