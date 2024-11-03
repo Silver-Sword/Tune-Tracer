acfbf9ddc022f54e285394880b57aaed8468d95f
Jordan Muniz <jordy.valois@gmail.com>
2024-11-02 20:13:32 -0400

    Don't get Doc ID from cookie

--- frontend/src/app/composition_tool/sharing/sharing_api.ts
+++ frontend/src/app/composition_tool/sharing/sharing_api.ts
@@ -1,11 +1,11 @@
-import { getUserID, getDocumentID } from "../../cookie";
+import { getUserID } from "../../cookie";
 import { callAPI } from "../../../utils/callAPI";
 import { ShareStyle } from "../../lib/src/documentProperties";
 
-export const createShareCode = async (accessLevel: "Viewer" | "Editor") => {
+export const createShareCode = async (accessLevel: "Viewer" | "Editor", documentId: string) => {
   const sharing = accessLevel === "Viewer" ? ShareStyle.READ : ShareStyle.WRITE;
   const param = {
-    documentId: getDocumentID(),
+    documentId: documentId,
     sharing: sharing,
     writerId: getUserID(),
   };
@@ -13,10 +13,10 @@ export const createShareCode = async (accessLevel: "Viewer" | "Editor") => {
   return callAPI("createShareCode", param);
 };
 
-export const updateDocumentShareStyle = async (accessType: "restricted" | "anyone", accessLevel: "Viewer" | "Editor") => {
+export const updateDocumentShareStyle = async (accessType: "restricted" | "anyone", accessLevel: "Viewer" | "Editor", documentId: string) => {
   const sharing = accessType === "restricted" ? ShareStyle.NONE : accessLevel === "Viewer" ? ShareStyle.READ : ShareStyle.WRITE;
   const param = {
-    documentId: getDocumentID(),
+    documentId: documentId,
     sharing: sharing,
     writerId: getUserID(),
   };
