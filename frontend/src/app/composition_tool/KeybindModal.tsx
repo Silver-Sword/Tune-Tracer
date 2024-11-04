cb34e05cf8e3c4fd2cccc0d1b96b841bb8d910d5
JASCuyugan <jascuyugan@gmail.com>
2024-11-04 12:45:54 -0500

    Added more style to the keyboard shortcut modal

--- frontend/src/app/composition_tool/KeybindModal.tsx
+++ frontend/src/app/composition_tool/KeybindModal.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Modal, Text, Stack, Group } from "@mantine/core";
+import { Button, Modal, Text, Stack, Group, Kbd, Title } from "@mantine/core";
 
 export const KeybindModal: React.FC = () => {
   const [isHelpModalOpen, setHelpModalOpen] = useState(false);
@@ -21,7 +21,7 @@ export const KeybindModal: React.FC = () => {
       <Modal
         opened={isHelpModalOpen}
         onClose={closeHelpModal}
-        title="Keyboard Shortcuts"
+        title={<Title order={2}>Keyboard Shortcuts</Title>}
         centered
       >
         <Stack gap="md">
@@ -31,13 +31,23 @@ export const KeybindModal: React.FC = () => {
           
           <Group gap="xs">
             <Text>
-              <b>Note Keys:</b> Press any note key to add the corresponding note (e.g., "A" for A, "B" for B).
+              <b>Note Keys:</b>
+              <ul style={{ margin: 0, paddingLeft: "1.5rem" }}>
+                <li><Kbd>A</Kbd> - Adds an A note</li>
+                <li><Kbd>B</Kbd> - Adds a B note</li>
+                <li><Kbd>C</Kbd> - Adds a C note</li>
+                <li><Kbd>D</Kbd> - Adds a D note</li>
+                <li><Kbd>E</Kbd> - Adds an E note</li>
+                <li><Kbd>F</Kbd> - Adds an F note</li>
+                <li><Kbd>G</Kbd> - Adds a G note</li>
+              </ul>
+            </Text>
+
+            <Text>
+              <b>Pitch Shift:</b> Press <Kbd>W</Kbd> to pitch shift up and <Kbd>S</Kbd> to pitch shift down.
             </Text>
             <Text>
-              <b>Pitch Shift:</b> Press <b>W</b> to pitch shift up and <b>S</b> to pitch shift down.
-            </Text>
-            <Text>
-              <b>Delete:</b> Press <b>Backspace</b> to delete the last note added.
+              <b>Delete</b>: Press <Kbd>Backspace</Kbd> to delete the last note added.
             </Text>
           </Group>
         </Stack>
