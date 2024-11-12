c73121176bee5fa696dfd3aed300a9ad272bf799
Silver-Sword <38964705+Silver-Sword@users.noreply.github.com>
2024-11-12 00:06:18 -0500

    fix bug and make max users displayed into constant

--- frontend/src/app/composition_tool/onlineUsers/OnlineUsersToolbarContainer.tsx
+++ frontend/src/app/composition_tool/onlineUsers/OnlineUsersToolbarContainer.tsx
@@ -3,7 +3,9 @@ import React from "react";
 import UserColorsModal from "./UserColorsModal";
 import OnlineUserIcon from "./OnlineUserIcon";
 
-import { Avatar, Group, MantineTheme, Tooltip } from "@mantine/core";
+import { Avatar, Group, Tooltip } from "@mantine/core";
+
+const MAX_USERS_DISPLAYED = 3;
 
 interface OnlineUsersToolbarContainerProps {
   modalOpened: boolean;
@@ -18,7 +20,7 @@ export const OnlineUsersToolbarContainer: React.FC<
   return (
     <Group>
       <Avatar.Group>
-        {userList.slice(0, 2).map((user) => (
+        {userList.slice(0, MAX_USERS_DISPLAYED).map((user) => (
           <OnlineUserIcon
             color={user.color}
             size={40}
@@ -27,7 +29,7 @@ export const OnlineUsersToolbarContainer: React.FC<
           />
         ))}
 
-        {userList.length > 3 && (
+        {userList.length > MAX_USERS_DISPLAYED && (
           <Tooltip label="See all users" withArrow>
             <Avatar
               color="gray"
